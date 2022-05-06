3f9797d11bd196eef1d67588e26c2ecaf8edc399
devYeonjun <alcls0026@gmail.com>
2022-05-06 23:16:55 +1200

    Add Hook register form to Redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
-import {toast} from 'react-toastify'
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+
+import { register } from "../features/auth/authSlice";
 
 function Register() {
 	const [formData, setFormData] = useState({
@@ -12,21 +15,34 @@ function Register() {
 
 	const { name, email, password, password2 } = formData;
 
-    const onChange = (e) => {
-        setFormData((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value,
-        }))
-    }
+	const dispatch = useDispatch();
 
-    const onSubmit = (e) => {
-        e.preventDefault()
+	const { user, isLoading, isSuccess, message } = useSelector(
+		(state) => state.auth
+	);
 
+	const onChange = (e) => {
+		setFormData((prevState) => ({
+			...prevState,
+			[e.target.name]: e.target.value,
+		}));
+	};
 
-        if(password != password2) {
-            toast.error('passwords do not mastch')
-        }
-    }
+	const onSubmit = (e) => {
+		e.preventDefault();
+
+		if (password != password2) {
+			toast.error("passwords do not mastch");
+		} else {
+			const userData = {
+				name,
+				email,
+				password,
+			};
+
+			dispatch(register(userData));
+		}
+	};
 
 	return (
 		<>
@@ -44,52 +60,52 @@ function Register() {
 							type="text"
 							className="form-control"
 							id="name"
-                            name='name'
+							name="name"
 							value={name}
 							onChange={onChange}
 							placeholder="Enter your name"
-                            required
+							required
 						/>
 					</div>
-                    <div className="form-group">
+					<div className="form-group">
 						<input
 							type="email"
 							className="form-control"
 							id="email"
-                            name='email'
+							name="email"
 							value={email}
 							onChange={onChange}
 							placeholder="Enter your email"
-                            required
+							required
 						/>
 					</div>
-                    <div className="form-group">
+					<div className="form-group">
 						<input
 							type="password"
 							className="form-control"
 							id="password"
-                            name='password'
+							name="password"
 							value={password}
 							onChange={onChange}
 							placeholder="Enter your password"
-                            required
+							required
 						/>
 					</div>
-                    <div className="form-group">
+					<div className="form-group">
 						<input
 							type="password2"
 							className="form-control"
 							id="password2"
-                            name='password2'
+							name="password2"
 							value={password2}
 							onChange={onChange}
 							placeholder="Confirm password"
-                            required
+							required
 						/>
 					</div>
-                    <div className="form-group">
-                        <button className="btn btn-block">Submit</button>
-                    </div>
+					<div className="form-group">
+						<button className="btn btn-block">Submit</button>
+					</div>
 				</form>
 			</section>
 		</>
